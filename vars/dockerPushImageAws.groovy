6dbdf45d3eebc6c825fd74bcdb5317ba7eb53d10
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 12:03:46 +0200

    Added docker push to AWS

---
+++ vars/dockerPushImageAws.groovy
@@ -0,0 +1,20 @@
+def call(Map PushConfig){
+
+        withAWS(region:"${PushConfig.awsRegion}",credentials:"${PushConfig.awsCredId}"){
+                script {
+                        def appName = PushConfig.pushImageTag.split(':')[2].trim()
+                        //def create_repo_cmd = sh script: "aws ecr create-repository --repository-name ${APP_NAME}"
+                        def docker_login_cmd = sh script: "aws ecr get-login --no-include-email", returnStdout: true
+                        def docker_login_endpt = login_cmd.split(' ')[7].trim()
+
+                        imgAwsTag = "${docker_login_endpt.split('//')[1]}/${PushConfig.pushImageTag}"
+
+                        sh script: """
+                            ${docker_login_cmd}
+                            docker tag ${PushConfig.localImageTag} ${imgAwsTag}
+                            docker push ${imgAwsTag}
+                            """, returnStdout: true
+                    }
+        }
+
+}
