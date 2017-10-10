23da7b10c83b3952a5fcb9c08bc772015296231d
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 09:20:33 +0200

    Added body() to function

--- vars/dockerPushImage.groovy
+++ vars/dockerPushImage.groovy
@@ -1,13 +1,15 @@
 
 
-def call(registryUrl,imageOrigName,imagePushName){ 
-        withDockerRegistry(url:"${registryUrl}",credentialsId:"local-docker-registry"){
-				script{
-						imgLocalTag = "registry.madisonmk.com/${imageName}"
-						sh script: """
-                            docker tag ${imageOrigName} ${imgLocalTag}
-                            docker push ${imgLocalTag}
-                            """, returnStdout: true
-				}
-		}
+def call(Closure body){ 
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+
+    body()
+
+    echo config.dockerImage
+    echo config.dockerRegistry
 }
+
+
