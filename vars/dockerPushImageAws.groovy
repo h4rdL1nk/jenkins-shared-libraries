62293d005942698a670924d34164469f81c9798f
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 12:20:11 +0200

    Fixed IndexOutOfBounds exception

--- vars/dockerPushImageAws.groovy
+++ vars/dockerPushImageAws.groovy
@@ -5,7 +5,7 @@ def call(Map PushConfig){
                         def appName = PushConfig.pushImageTag.split(':')[1].trim()
                         //def create_repo_cmd = sh script: "aws ecr create-repository --repository-name ${APP_NAME}"
                         def docker_login_cmd = sh script: "aws ecr get-login --no-include-email", returnStdout: true
-                        def docker_login_endpt = docker_login_cmd.split(' ')[7].trim()
+                        def docker_login_endpt = docker_login_cmd.split(' ')[6].trim()
 
                         imgAwsTag = "${docker_login_endpt.split('//')[1]}/${PushConfig.pushImageTag}"
 
