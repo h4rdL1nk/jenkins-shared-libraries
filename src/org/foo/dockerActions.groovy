a88266d7dd459de3363cd8e5b73c7ba2df0b5913
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 07:33:50 +0200

    Added dockerActions library

---
+++ src/org/foo/dockerActions.groovy
@@ -0,0 +1,13 @@
+
+def pushDockerImage(){ 	
+		withDockerRegistry(url:'https://registry.madisonmk.com',credentialsId:"local-docker-registry"){
+				script{
+						imgTag = codeCo.GIT_COMMIT
+						imgLocalTag = "registry.madisonmk.com/${DEPARTMENT}/${APP_NAME}:${imgTag}"
+						sh script: """
+                            docker tag jenkins-${JOB_NAME}-${BUILD_NUMBER}-img ${imgLocalTag}
+                            docker push ${imgLocalTag}
+                            """, returnStdout: true
+				}
+		}
+}
