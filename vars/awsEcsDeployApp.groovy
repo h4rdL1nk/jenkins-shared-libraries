f7cc3d959dccc451c9ab070bac68d143cb219dda
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 16:01:20 +0200

    Added TaskDefinition formatting

--- vars/awsEcsDeployApp.groovy
+++ vars/awsEcsDeployApp.groovy
@@ -8,11 +8,40 @@ def call(Map DeployConfig){
 
                         sh script: """
                                 clArn=\$(aws ecs list-clusters | jq -r '.clusterArns[]|select(test("^.*CL.*-'${awsEnv}'\$"))')
+
                                 svcArn=\$(aws ecs list-services --cluster \${clArn} \
                                           | jq -r '.serviceArns[]|select(test("^.*SVC-'${awsAppName}'"))')
+
                                 svcTaskDefArn=\$(aws ecs describe-services  --cluster \${clArn} --services \${svcArn} \
                                           | jq -r '.services[].taskDefinition')
-                                echo \${svcTaskDefArn}
+
+                                svcTaskDefJson=\$(aws ecs describe-task-definition --task-definition \${svcTaskDefArn})
+
+                                modSvcTaskDefJson=\$(echo \${svcTaskDefJson} \
+                                    | jq -r '.taskDefinition|
+                                            if .volumes 
+                                                    then .volumes=.volumes 
+                                                    else .volumes=[] 
+                                                    end|
+                                            if .placementConstraints != null 
+                                                    then .placementConstraints=.placementConstraints 
+                                                    else .placementConstraints=[] 
+                                                    end|
+                                            .containerDefinitions[].image="'${awsEcrImg}'"|
+                                            if .taskRoleArn 
+                                                    then .taskRoleArn=.taskRoleArn 
+                                                    lse .taskRoleArn=null 
+                                                    end|
+                                            {
+                                                networkMode:.networkMode,
+                                                family:.family,
+                                                volumes:.volumes,
+                                                containerDefinitions:.containerDefinitions,
+                                                taskRoleArn:.taskRoleArn,
+                                                placementConstraints:.placementConstraints
+                                            }')
+
+                                echo \${modSvcTaskDefJson} | jq -r ''
                             """
                 }
         }
