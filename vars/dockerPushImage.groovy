29b63276a42cf23c54a3f162915f0969c59d0c90
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 11:45:44 +0200

    Added registry endpoint

--- vars/dockerPushImage.groovy
+++ vars/dockerPushImage.groovy
@@ -4,9 +4,10 @@ def call(Map PushConfig){
 
         withDockerRegistry(url:"${PushConfig.registryUrl}",credentialsId:"${PushConfig.registryCredId}"){
         script{ 
+                def registryEndpoint = PushConfig.registryUrl.split('//')[1].trim()
                 sh script: """
                         docker tag ${PushConfig.localImageTag} ${PushConfig.pushImageTag}
-                        docker push ${PushConfig.pushImageTag}
+                        docker push ${registryEndpoint}/${PushConfig.pushImageTag}
                     """, returnStdout: true
                 }
         }
