e845dc4261dd057c15cb037a1858697604063c26
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 11:19:06 +0200

    Added newline

--- vars/dockerPushImage.groovy
+++ vars/dockerPushImage.groovy
@@ -5,9 +5,11 @@ def call(Map PushConfig){
         withDockerRegistry(url:"${PushConfig.registryUrl}",credentialsId:"${PushConfig.registryCredId}"){
         script{
                 sh script: """
+
                         docker tag ${PushConfig.localImageTag} ${PushConfig.pushImageTag}
                         docker push ${PushConfig.pushImageTag}
-                """, returnStdout: true
+
+                    """, returnStdout: true
                 }
         }
 
