52e3fcd4dc8497599885f3f76df7d099c78fd19c
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 15:18:10 +0200

    Fixed echoes

--- vars/awsEcsDeployApp.groovy
+++ vars/awsEcsDeployApp.groovy
@@ -3,8 +3,8 @@ def call(Map DeployConfig){
         withAWS(region:"${DeployConfig.awsRegion}",credentials:"${DeployConfig.awsCredId}"){
                 script{
                         awsEnv = "${DeployConfig.awsAppEnv}"
-                        awsAppName = "${DeployConfig.awsAppEnv}"
-                        awsEcrImg = "${DeployConfig.awsAppEnv}"
+                        awsAppName = "${DeployConfig.awsAppName}"
+                        awsEcrImg = "${DeployConfig.awsEcrImg}"
 
                         sh script: """
                                 echo ENV: ${awsEnv}
