60aa53d8ec59141c79ed41d7eda8732f89405b3a
h4rdL1nk <imthedanger@protonmail.com>
2017-10-10 13:27:07 +0200

    Updated AWS/ECS deploy

--- vars/awsEcsDeployApp.groovy
+++ vars/awsEcsDeployApp.groovy
@@ -6,28 +6,17 @@ def call(Map DeployConfig){
                         awsAppName = "${DeployConfig.awsAppEnv}"
                         awsEcrImg = "${DeployConfig.awsAppEnv}""
 
-                        def AwsCluster = sh script: """
+                        sh script: """
+                            #!/bin/bash
+
                             aws ecs list-clusters | jq -r '.clusterArns[]|select(test("^.*CL.*-${awsEnv}\$"))'
+                            
+                            #aws ecs list-services --cluster ${AwsCluster.trim()} | jq -r '.serviceArns[]|select(test("^.*:service/SVC-${awsAppName}(-${awsEnv})?.*\$"))'
+                            #aws ecs describe-services --cluster ${AwsCluster.trim()} --services ${AwsService.trim()} | jq -r '.services[].deployments[].taskDefinition'
+                            #aws ecs describe-task-definition --task-definition ${AwsTaskDef.trim()} | jq -rc '.taskDefinition|.containerDefinitions[].image="'${awsEcrImg}'"|if .volumes != null then .volumes=.volumes else .volumes=[] end|if .networkMode != null then .networkMode=.networkMode else .networkMode="bridge" end|if .placementConstraints != null then .placementConstraints=.placementConstraints else .placementConstraints=[] end|{family:.family,containerDefinitions:.containerDefinitions,volumes:.volumes,placementConstraints:.placementConstraints,networkMode:.networkMode}'
+                            #aws ecs register-task-definition --cli-input-json '${AwsTaskDefJson.trim()}' | jq -r '.taskDefinition.taskDefinitionArn'
+                            #aws ecs update-service --cluster ${AwsCluster.trim()} --service ${AwsService.trim()} --task-definition ${AwsTaskDefArn.trim()} | jq -r '.service.taskDefinition'
                             """, returnStdout: true
-                        def AwsService = sh script: """
-                            aws ecs list-services --cluster ${AwsCluster.trim()} | jq -r '.serviceArns[]|select(test("^.*:service/SVC-${awsAppName}(-${awsEnv})?.*\$"))'
-                            """, returnStdout: true
-                        def AwsTaskDef = sh script: """
-                            aws ecs describe-services --cluster ${AwsCluster.trim()} --services ${AwsService.trim()} | jq -r '.services[].deployments[].taskDefinition'
-                            """, returnStdout: true
-                        def AwsTaskDefJson = sh script: """
-                            aws ecs describe-task-definition --task-definition ${AwsTaskDef.trim()} | jq -rc '.taskDefinition|.containerDefinitions[].image="'${awsEcrImg}'"|if .volumes != null then .volumes=.volumes else .volumes=[] end|if .networkMode != null then .networkMode=.networkMode else .networkMode="bridge" end|if .placementConstraints != null then .placementConstraints=.placementConstraints else .placementConstraints=[] end|{family:.family,containerDefinitions:.containerDefinitions,volumes:.volumes,placementConstraints:.placementConstraints,networkMode:.networkMode}'
-                            """, returnStdout: true
-                        def AwsTaskDefArn = sh script: """
-                            aws ecs register-task-definition --cli-input-json '${AwsTaskDefJson.trim()}' | jq -r '.taskDefinition.taskDefinitionArn'
-                            """, returnStdout: true
-                        def AwsSvcUpdatedTask = sh script: """
-                            aws ecs update-service --cluster ${AwsCluster.trim()} --service ${AwsService.trim()} --task-definition ${AwsTaskDefArn.trim()} | jq -r '.service.taskDefinition'
-                            """, returnStdout: true
-
-                        echo "Service updated with ${AwsSvcUpdatedTask.trim()}"
-
-                        echo "Finished build ${JOB_NAME}:${BUILD_NUMBER} at node ${NODE_NAME}"
                 }
         }
 }
