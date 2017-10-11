714ba6e05571cfa078d36e4f037fcd6ff8de7ee2
h4rdL1nk <imthedanger@protonmail.com>
2017-10-11 12:24:24 +0200

    Fixed exit commands

--- vars/awsEcsDeployApp.groovy
+++ vars/awsEcsDeployApp.groovy
@@ -11,24 +11,24 @@ def call(Map DeployConfig){
 
                                 clArn=\$(aws ecs list-clusters | jq -r '.clusterArns[]|select(test("^.*CL.*-'${awsEnv}'\$"))' 2>/dev/null)
 
-                                [ \$? -ne 0 ] && echo error recuperando ECS/Cluster && 2
+                                [ \$? -ne 0 ] && echo error recuperando ECS/Cluster && exit 2
                                 [ -z \${clArn} ] && echo ECS/Cluster no encontrado && exit 1
 
                                 svcArn=\$(aws ecs list-services --cluster \${clArn} \
                                           | jq -r '.serviceArns[]|select(test("^.*SVC-'${awsAppName}'"))' 2>/dev/null)
 
-                                [ \$? -ne 0 ] && echo error recuperando ECS/Service && 2
+                                [ \$? -ne 0 ] && echo error recuperando ECS/Service && exit 2
                                 [ -z \${svcArn} ] && echo ECS/Service no encontrado && exit 1
 
                                 svcTaskDefArn=\$(aws ecs describe-services  --cluster \${clArn} --services \${svcArn} \
                                           | jq -r '.services[].taskDefinition' 2>/dev/null)
 
-                                [ \$? -ne 0 ] && echo error recuperando ECS/TaskDefinition && 2
+                                [ \$? -ne 0 ] && echo error recuperando ECS/TaskDefinition && exit 2
                                 [ -z \${svcTaskDefArn} ] && echo ECS/TaskDefinition no encontrada && exit 1
 
                                 svcTaskDefJson=\$(aws ecs describe-task-definition --task-definition \${svcTaskDefArn} 2>/dev/null)                                
 
-                                [ \$? -ne 0 ] && echo error recuperando ECS/TaskDefinition && 2
+                                [ \$? -ne 0 ] && echo error recuperando ECS/TaskDefinition && exit 2
 
                                 modSvcTaskDefJson=\$(echo \${svcTaskDefJson} \
                                     | jq -r '.taskDefinition|
@@ -53,12 +53,12 @@ def call(Map DeployConfig){
                                                 placementConstraints:.placementConstraints
                                             }')
 
-                                [ \$? -ne 0 ] && echo error construyendo ECS/TaskDefinition && 2
+                                [ \$? -ne 0 ] && echo error construyendo ECS/TaskDefinition && exit 2
 
                                 svcNewTaskDefArn=\$(aws ecs register-task-definition --cli-input-json "\${modSvcTaskDefJson}" \
                                     | jq -r '.taskDefinition.taskDefinitionArn' 2>/dev/null)
 
-                                [ \$? -ne 0 ] && echo error registrando ECS/TaskDefinition && 2
+                                [ \$? -ne 0 ] && echo error registrando ECS/TaskDefinition && exit 2
 
                                 svcUpdateResult=\$(aws ecs update-service \
                                     --cluster \${clArn} \
@@ -66,7 +66,7 @@ def call(Map DeployConfig){
                                     --task-definition \${svcNewTaskDefArn} \
                                     | jq -r '.service.deployments[]|select(.status=="PRIMARY")|.taskDefinition' 2>/dev/null)
 
-                                [ \$? -ne 0 ] && echo error actualizando ECS/Service && 2
+                                [ \$? -ne 0 ] && echo error actualizando ECS/Service && exit 2
                             """
                 }
         }
